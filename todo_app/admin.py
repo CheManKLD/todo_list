6952479b89c91ef2ed3639592c72693917a23d65
CheManKLD <cherry_diman@mail.ru>
2022-04-27 17:51:13 +0200

    Создал модели списка дел и пунктов к данному списку, добавил их в админку

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(ToDoList)
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('title',)
+    search_fields = ('title',)
+
+
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'due_date', 'title', 'todo_list')
+    list_filter = ('todo_list', 'due_date')
+    search_fields = ('title',)
