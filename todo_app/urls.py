ddd8b37c8aa296da7f877b8771e6399f6d5e80fa
CheManKLD <cherry_diman@mail.ru>
2022-05-21 20:26:00 +0200

    Добавил View для перечня дел и переход на него с главной страницы

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ToDoListView.as_view(), name='index')
+    path('', views.ToDoListView.as_view(), name='index'),
+    path('list/<int:list_id>/', views.ToDoItemView.as_view(), name='list'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -6,3 +6,17 @@ from .models import *
 class ToDoListView(ListView):
     model = ToDoList
     template_name = 'todo_app/index.html'
+    extra_context = {'list_selected': 0}
+
+
+class ToDoItemView(ListView):
+    model = ToDoItem
+    template_name = 'todo_app/todo_list.html'
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs['list_id'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['todo_list'] = ToDoList.objects.get(id=self.kwargs['list_id'])
+        return context
